d39c3a5f59df472e97dd44126b135755985e8104
Samuel Obeng <sammieobeng50@gmail.com>
2025-09-20 21:50:44 +0000

    Initial 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
+      <MainContent />
       <WelcomeMessage /> 
+      <Footer />
     </>
   )
 }
