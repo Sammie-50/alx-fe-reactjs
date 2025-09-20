6e55912ac59392374d9281b17f3107ebc28be3b3
Samuel Obeng <sammieobeng50@gmail.com>
2025-09-20 22:40:53 +0000

    Added a Counter file

---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,27 @@
+
+import { useState } from "react";
+
+function Counter() {
+  // state variable to track count, initialized to 0
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <p style={{ fontSize: "20px" }}>Current Count: {count}</p>
+
+      <button onClick={() => setCount(count + 1)} style={{ margin: "5px" }}>
+        Increment
+      </button>
+
+      <button onClick={() => setCount(count - 1)} style={{ margin: "5px" }}>
+        Decrement
+      </button>
+
+      <button onClick={() => setCount(0)} style={{ margin: "5px" }}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
