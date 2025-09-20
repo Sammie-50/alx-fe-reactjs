6ab70977cfebf1e87fa323ef7863723712d55da5
Samuel Obeng <sammieobeng50@gmail.com>
2025-09-20 22:55:57 +0000

    Fixed UserContext error

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import { useState } from "react";
+import "./App.css";
+import WelcomeMessage from "./Components/WelcomeMessage.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import MainContent from "./components/MainContent.jsx";
+import UserProfile from "./components/UserProfile.jsx";
+import ProfilePage from "./ProfilePage";
+import UserContext from "./components/UserContext.js";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
